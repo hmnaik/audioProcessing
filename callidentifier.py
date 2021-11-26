333a1bd7af31c3c68f7f18e18f4616fbc345d043
Hemal_cluster_workstation <hmnaiks@gmail.com>
2021-11-26 18:37:04 +0100

    first commit

---
+++ callidentifier.py
@@ -0,0 +1,128 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import os
+import numpy as np
+from matplotlib import pyplot as plt
+import IPython.display as ipd
+import librosa
+import glob
+import csv
+import pandas as pd
+
+# matplotlib inline
+def print_plot_play(x, Fs, text=''):
+    """1. Prints information about an audio singal, 2. plots the waveform, and 3. Creates player
+
+    Notebook: C1/B_PythonAudio.ipynb
+
+    Args:
+        x: Input signal
+        Fs: Sampling rate of x
+        text: Text to print
+    """
+    #print('%s Fs = %d, x.shape = %s, x.dtype = %s' % (text, Fs, x.shape, x.dtype))
+    plt.figure(figsize=(8, 2))
+    plt.plot(x, color='gray')
+    plt.xlim([0, x.shape[0]])
+    plt.ylim([-0.2, 0.2])
+    plt.xlabel('Time (samples)')
+    plt.ylabel('Amplitude')
+    plt.tight_layout()
+    plt.savefig(text+".jpg")
+   # plt.show()
+    ipd.display(ipd.Audio(data=x, rate=Fs))
+
+def read_audio(name):
+    # Use a breakpoint in the code line below to debug your script.
+    #fn_wav = os.path.join('..', 'data', 'B', 'D:\\Barn Stuff\\trial1_Chan07_2019-12-09_11-15-00_ADC05000mV_00dB.wav')
+    fn_wav = str(name)
+    x, Fs = librosa.load(fn_wav, sr=None)
+    #print_plot_play(x=x, Fs=Fs, text=f'{name}')
+    maxAmp = max(x)
+    x = list(x)
+    sampleNo = x.index(maxAmp)
+    return max(x), sampleNo
+
+def read_files(path):
+    # return all the wav files in the path as list
+    filenames= glob.glob(os.path.join(path, "*.wav"))
+    return filenames
+
+def plot_intensities(ampDict):
+    lists = ampDict.items()
+    x,y = zip(*lists)
+    x = range(len(x))
+    plt.figure(figsize=(8, 2))
+    #plt.plot(x, color='gray')
+    plt.plot(x, y)
+    plt.xlim([0, len(x) ]) #x.shape[0]])
+    plt.ylim([-0.2, 0.2])
+    plt.xlabel('Samples')
+    plt.ylabel('Amplitude')
+    plt.tight_layout()
+
+    #plt.show()
+    plt.savefig("plotIntensities.jpg")
+
+def findInfoFromName(audioFileName):
+    dict = {}
+
+    sections = audioFileName.split("_")
+    channelSection = sections[1].split("Chan")
+    dateSection = sections[2].split("-")
+    timeSection = sections[3].split("-")
+
+    dict["Channel"] = channelSection[1]
+    dict["Year"] = dateSection[0]
+    dict["Month"] = dateSection[1]
+    dict["Day"] = dateSection[2]
+    dict["Hour"] = timeSection[0]
+    dict["Minute"] = timeSection[1]
+    dict["Seconds_5"] = timeSection[2]
+
+    return dict
+
+def create_empty_dict(list):
+    dict = {}
+
+    for i in list:
+        dict[i] = 0
+
+    return dict
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    # Path for the audio files
+    path = "D:\\Barn Stuff\\AudioSamples"
+    # get list of all the audio files (*.wav) from the given path
+    filenames = read_files(path)
+
+    # filename for storing data
+    csvFile = "dataBase.csv"
+    csvFilePath = os.path.join(path,csvFile)
+    print(f'Path:{csvFilePath}')
+
+    # Create handler for csv storage file and write header
+    csvFileHandler = open(csvFilePath, "w", newline="")
+    csvWriter = csv.writer(csvFileHandler, delimiter = ",")
+    header = ["Filename", "Channel", "Year", "Month", "Day", "Hour", "Minute","Seconds_5", "Max_intensity", "Max_sample_timing"]
+    csvWriter.writerow(header)
+
+    # Create empty dict
+    dataDict = create_empty_dict(header)
+    # Go through each file and extract call amplitudes etc.
+    for name in filenames:
+        dateTimeInfo_dict = findInfoFromName(name)
+        dataDict.update(dateTimeInfo_dict)
+        dataDict["Filename"] = os.path.basename(name)
+        dataDict["Max_intensity"], dataDict["Max_sample_timing"] = read_audio(name)
+        print(f'File:{name}')
+        # Get all values as list
+        values = list(dataDict.values())
+        csvWriter.writerow(values)
+
+    #close the file
+    csvFileHandler.close()
---
+++ readTextFile.py
@@ -0,0 +1,76 @@
+import math
+import csv
+
+
+def find_time_stamp(fifteentime):
+    mins = math.floor( fifteentime / 60)
+    totalSecs = math.floor(fifteentime - (mins * 60))
+    seconds = math.floor(totalSecs/5)*5
+    return mins, seconds
+
+def read_File(filename):
+    print("Read files")
+    filename_without_extension = filename.split(".txt")[0]
+    filename_with_extension = filename_without_extension + "_modified" + ".txt"
+    csvFileName = filename_without_extension + "_modified" + ".csv"
+
+    csvFileHandler = open(csvFileName,'w', newline= "")
+    csvWriter = csv.writer(csvFileHandler, delimiter = ",")
+    #
+    dataFile = open(filename_with_extension, "w+")
+    # For identification of file names
+    wavFilePrefix = "trial1_Chan14_2019-12-09_"
+    wavFileSuffix = "_ADC05000mV_00dB.wav"
+    lineCount = 0
+
+    with open(filename) as file:
+        lines = file.readlines()
+        # Go through everyline
+        for line in lines:
+            line = line.rstrip()
+            listOfValues = line.split("\t")
+            # find the lines with values (Not header and not empty lines)
+            if lineCount == 0:
+                listOfValues.append("soundFileName")
+                csvWriter.writerow(listOfValues)
+
+            elif len(listOfValues)>1 and lineCount != 0:
+                # Find time of call within 15 min session (Mins and Seconds)
+                timeOfCall = float(listOfValues[3])
+                call_mins, base_seconds = find_time_stamp(timeOfCall)
+                # Find the correct 15 min segment to add mins and seconds
+                time_stamp = listOfValues[6].split("_")
+                base_hour = int(time_stamp[0])  # Extract base hour value
+                base_mins = int(time_stamp[1])  # Extract base min value
+                total_mins = base_mins + call_mins
+                #listOfValues.append(str(hour))
+                #listOfValues.append(str(basemins + mins))
+                if base_seconds < 10:
+                    base_seconds = "0" + str(base_seconds)
+                #listOfValues.append(str(base_seconds) )
+                # Create name of the audio file
+                audioFileName = wavFilePrefix + str(base_hour) + "-" + str(total_mins) + "-" + str(base_seconds) + wavFileSuffix
+                listOfValues.append(audioFileName)
+
+                # Replace time and date with actual values
+                date, time = listOfValues[5].split("_")
+                updatedDateTime = date + "_" + str(base_hour) + "-" + str(total_mins) + "-" + str(base_seconds)
+                listOfValues[5] = updatedDateTime
+
+                print(listOfValues)
+                dataFile.write(str(listOfValues))
+                dataFile.write("\n")
+                # Write in CSV writer
+                csvWriter.writerow(listOfValues)
+            else:
+                print("Error ")
+
+            lineCount = lineCount + 1
+        #
+        dataFile.close()
+
+if __name__ == '__main__':
+    """
+    """
+    name = "D:\\Barn Stuff\\VerificationFile\\trial1_Chan14_2019-12-09_11-15-00to13-00_OnlyVerrifiedCalls.txt"
+    read_File(name)
