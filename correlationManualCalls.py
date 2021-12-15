1c58ae96b776f3a66088c9c828632bb4f81055b3
Hemal_cluster_workstation <hmnaiks@gmail.com>
2021-12-15 16:59:20 +0100

    *Commit

--- correlationManualCalls.py
+++ correlationManualCalls.py
@@ -4,16 +4,16 @@ import pandas as pd
 import librosa
 import numpy as np
 from scipy import signal
+from matplotlib import pyplot as plt
 
 def getSampleData(file, minRange, maxRange):
     #work out padding
     samples, Fs = librosa.load(file, sr=None)
     samples = samples[minRange:maxRange]
-    print(f"Size: {len(samples)}")
+    #print(f"Size: {len(samples)}")
     return samples
 
 def find_correlation_points(soundFiles, sampleTiming, focalChannel, path_to_files):
-    print("Temp")
     correlationDataDict = {}
     lagDataDict = {}
 
@@ -28,41 +28,77 @@ def find_correlation_points(soundFiles, sampleTiming, focalChannel, path_to_file
     for file in soundFiles:
         # Select the file that is not focal file
         if file == focalFile:
-            print("File")
+            #print("File")
             correlation = 0
             correlationDataDict[file] = correlation
         else:
             queryFilePath = os.path.join(path_to_files, file)
             dataOnQueryFile = getSampleData(queryFilePath, sampleTiming-min_sample_range, sampleTiming+max_sample_range)
-            corr_values= signal.correlate(dataOnFocalFile, dataOnQueryFile, mode="full", method= "auto")
+
+            #plot
+
+
+            corr_values= signal.correlate( dataOnQueryFile, dataOnFocalFile , mode="full", method= "auto")
             maxCorr = np.max(corr_values)
-            print(corr_values)
+
+            #plot
+
+            #print(corr_values)
             lags = signal.correlation_lags(len(dataOnFocalFile),len(dataOnQueryFile), mode = "full")
             lag = lags [np.argmax(corr_values)]
-
             #corr_values = numpy.correlate(dataOnFocalFile,dataOnQueryFile)
-            print(f"Corrleation: {corr_values}")
+            #print(f"Corrleation: {corr_values}")
+            # fig, (ax_orig, ax_noise, ax_corr) = plt.subplots(3, 1, sharex=True)
+            # ax_orig.plot(dataOnFocalFile)
+            #
+            # #ax_orig.plot(clock, sig[clock], 'ro')
+            #
+            # ax_orig.set_title('Focal signal ')
+            #
+            # ax_noise.plot(dataOnQueryFile)
+            #
+            # ax_noise.set_title('Query signal')
+            #
+            # ax_corr.plot(corr_values)
+            #
+            # #ax_corr.plot(clock, corr[clock], 'ro')
+            #
+            # #ax_corr.axhline(0.5, ls=':')
+            #
+            # ax_corr.set_title('Cross-correlated signal')
+            #
+            # #ax_orig.margins(0, 0.1)
+            #
+            # fig.tight_layout()
+            #
+            # plt.show()
+
             correlationDataDict[file] = maxCorr
             lagDataDict[file] = lag
+
+            print("Waiting")
 
     return correlationDataDict,lagDataDict
 
 def process_dataset(dataSet, noOfChannels, path_to_files):
 
-    print("Information")
     # Take subset of first 23 channels
     totalSequence = int(dataSet.shape[0]/noOfChannels)
 
     # Create the list of indexes
-    indexList = [i for i in range(0,dataSet.shape[0],totalSequence)]
+    indexList = [i for i in range(0,dataSet.shape[0],noOfChannels)]
 
     list_of_correlation = []
     list_of_lag = []
 
     # Go through each index on the dataset and select all the channels
     for index in indexList:
+        print(f"index: {index}")
+        if index == 528:
+            print("Now")
         # Get the subset of the dataset
         subsetDataFrame = dataSet.iloc[index : index + noOfChannels]
+        print(f"Size: {subsetDataFrame.shape}")
         # Find the point with max intensity
         idMaxSample = subsetDataFrame["Max_intensity"].idxmax()
         # From the id get the sample point
@@ -72,28 +108,25 @@ def process_dataset(dataSet, noOfChannels, path_to_files):
 
         #list of files
         files = subsetDataFrame["Filename"].tolist()
-        min_sample_range = 1000
-        max_sample_range = 3000
+        # min_sample_range = 1000
+        # max_sample_range = 3000
 
         dataDict, lagDict = find_correlation_points(files, sampleTiming, focalChannel, path_to_files)
         listOfCorrVal = list(dataDict.values())
         list_of_correlation = list_of_correlation + listOfCorrVal
-        print(list_of_correlation)
+        #print(len(list_of_correlation))
 
         listOfLagVal = list(lagDict.values())
         list_of_lag = list_of_lag + listOfLagVal
+
+        print(f" lag values :{len(list_of_lag)} - lag values in the session : {len(listOfLagVal)}")
+
 
     # Find max intensity value and corresponding sample value (index)
     dataSet["Correlation"] = list_of_correlation
     dataSet["Lag"] = list_of_lag
     # Define sample range around the sample value
     return dataSet
-    # Get sample value of all such sound files and file correlation factor with those files
-
-    # Save focal value, correlation value and the time lag
-
-    # Write the information in the new file
-
 
 
 
@@ -108,4 +141,4 @@ if __name__ == '__main__':
     noOfChannels = 23
     updated_dataset = process_dataset(dataSet, noOfChannels, path_to_files)
     final_file = os.path.join(path_to_files, "correlation.csv")
-    updated_dataset.to_csv(final_file)
+    updated_dataset.to_csv(final_file, index = False)
