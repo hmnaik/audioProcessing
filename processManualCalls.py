8a5def6e994d8ff1fac7e2c9fa6b367749cd91f1
Hemal_cluster_workstation <hmnaiks@gmail.com>
2021-12-15 17:01:00 +0100

    *Adding the processing of corrleation data

---
+++ processManualCalls.py
@@ -0,0 +1,9 @@
+import math
+import csv
+
+
+if __name__ == '__main__':
+    """
+    """
+    name = "D:\\Barn Stuff\\VerificationFile\\trial1_Chan14_2019-12-09_11-15-00to13-00_OnlyVerrifiedCalls.txt"
+   
