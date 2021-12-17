cbced06ce52aa3d785d9b7da91e11e0f5391b94d
Hemal_cluster_workstation <hmnaiks@gmail.com>
2021-12-17 16:52:12 +0100

    *Manual files from Nora

--- processManualCalls.py
+++ processManualCalls.py
@@ -1,9 +1,23 @@
 import math
 import csv
+import pandas as pd
+
+#Process
+
+# Find time from the acoustic file
+
+# Find the right folder for the audio files
+
+def process_data(dataFrame):
+
+    for all in dataFrame:
+        print("hehe")
 
 
 if __name__ == '__main__':
     """
     """
-    name = "D:\\Barn Stuff\\VerificationFile\\trial1_Chan14_2019-12-09_11-15-00to13-00_OnlyVerrifiedCalls.txt"
-   
+    name = "D:\\Barn Stuff\\VerificationFile\\trial1_Chan14_2019-12-09_11-15-00to13-00_OnlyVerrifiedCalls_modified.csv"
+    dataFrame = pd.read_csv(name)
+
+    process_data(dataFrame)
